e0723b8dd26bbee773ddef2c3a11159cb24f1f2f
Shawn Freeman <sfreeman@sparksgroupinc.com>
2022-09-07 10:48:38 -0400

    WIP: Add kendo grid with data service binding

--- angular-kendo-ui/src/app/app.component.ts
+++ angular-kendo-ui/src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
+import { SortDescriptor } from '@progress/kendo-data-query';
+import { Observable } from 'rxjs';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   title = 'angular-kendo-ui';
+
+  //demo properties
+  public gridItems: Observable<GridDataResult>;
+  public pageSize: number = 10;
+  public skip: number = 0;
+  public sortDescriptor: SortDescriptor[] = [];
+  public filterTerm: number = null;
+
+  constructor(private service: ProductService){
+    this.loadGridItems();
+  }
+
+  public pageChange(event: PageChangeEvent){
+    this.skip = event.skip;
+    this.loadGridItems();
+  }
+
+  public handleSortChange(descriptor: SortDescriptor[]){
+    this.sortDescriptor = descriptor;
+    this.loadGridItems();
+  }
+
+  private loadGridItems():void{
+    this.gridItems = this.service.getProducts(
+      this.skip,
+      this.pageSize,
+      this.sortDescriptor,
+      this.filterTerm
+    );
+  }
 }
