4bbbdd238e46c054691fc65da949f3242d6c4d15
Shawn Freeman <sfreeman@sparksgroupinc.com>
2022-09-07 13:05:56 -0400

    Init reactJS project and abstracted game components into individual files

---
+++ react-kendo-ui/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Game from './game-component';
+import './index.css';
+  
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);
+  
