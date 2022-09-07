d7089af60364c3febef567e9d49527d0d884123d
Shawn Freeman <sfreeman@sparksgroupinc.com>
2022-09-07 11:27:51 -0400

    Drop down categories example

--- angular-kendo-ui/src/app/app.component.ts
+++ angular-kendo-ui/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
 import { DataResult, SortDescriptor } from '@progress/kendo-data-query';
 import { Observable, of } from 'rxjs';
+import { categories } from './data/data.categories';
 import { ProductService } from './product.service';
 
 @Component({
@@ -13,12 +14,16 @@ import { ProductService } from './product.service';
 export class AppComponent {
   title = 'angular-kendo-ui';
 
-  //demo properties
+  //grid demo properties
   public gridItems: Observable<GridDataResult> ;
   public pageSize: number = 10;
   public skip: number = 0;
   public sortDescriptor: SortDescriptor[] = [];
   public filterTerm: number = 0;
+
+  //dropdown demo properties
+  public dropDownItems = categories;
+  public defaultItem = { text: "Filter by Category", value: null as any };
 
   constructor(private service: ProductService){
     this.loadGridItems();
